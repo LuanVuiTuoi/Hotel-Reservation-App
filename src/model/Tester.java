bfd39ef1c203d64c8fdc1cc07070cb1bdcfdd6bc
LuanVuiTuoi <luanvuituoi@outlook.com>
2024-09-23 09:36:22 -0700

    Initial commit

---
+++ src/model/Tester.java
@@ -0,0 +1,19 @@
+package view;
+
+import model.Customer;
+import model.Reservation;
+import model.Room;
+import model.RoomType;
+
+import java.time.LocalDate;
+
+public class Tester {
+    public static void main(String[] args){
+        Customer customer = new Customer("first","last","j@domain.com");
+        System.out.println(customer);
+        Room room = new Room("101",10.0, RoomType.SINGLE);
+        System.out.println(room);
+        Reservation reservation = new Reservation(customer,room, LocalDate.now(),LocalDate.now());
+        System.out.println(reservation);
+    }
+}
